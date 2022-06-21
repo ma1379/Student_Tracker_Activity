0711980303c44c0fcd606c2526bff32ae9f7d7b0
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-21 11:28:22 -0500

    usernames and ages done

--- 01-starting-project/01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/users/AddUser.js
@@ -5,6 +5,15 @@ import Button from '../UI/Button';
 const AddUser = props => {
     function addUserHandler(event) {
         event.preventDefault();
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername("")
+        setEnteredAge("")
     }
     const [enteredUsername, setEnteredUsername] = useState("");
     const [enteredAge, setEnteredAge] = useState("");
@@ -18,9 +27,9 @@ const AddUser = props => {
         <Card>
         <form onSubmit={addUserHandler}>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='text' className='input' onChange={usernameChangeHandler}></input>
+            <input id='username' type='text' className='input' value={enteredUsername} onChange={usernameChangeHandler}></input>
             <label htmlFor='age' className='label'>Age (years)</label>
-            <input id='age' type='number' className='label' onChange={ageChangeHandler}></input>
+            <input id='age' type='number' className='label' value={enteredAge} onChange={ageChangeHandler}></input>
             <Button type='submit'>Add User</Button>
         </form>
         </Card>
