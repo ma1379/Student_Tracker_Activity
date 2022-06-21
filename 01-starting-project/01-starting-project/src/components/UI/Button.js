370c503c6487305a0177798468e91d3f1711f3a4
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-21 10:59:04 -0500

    revenge of the buttons

---
+++ 01-starting-project/01-starting-project/src/components/UI/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Button.css";
+
+const Button = props => {
+    return (
+        <div>
+        <button type = {props.type || "button"} onClick = {props.clickHandler}>{props.children}</button>
+        </div>
+    );
+}
+export default Button;
--- 01-starting-project/01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/users/AddUser.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import "./AddUser.css";
+import Card from "../UI/Card.js";
+import Button from '../UI/Button.js';
 
 const AddUser = props => {
     function addUserHandler(event) {
         event.preventDefault();
     }
     return (
-        <form onSubmit = {addUserHandler}>
-            <label htmlFor = 'username'>Username</label>
-            <input id = 'username' type = 'text'></input>
-            <label htmlFor = 'age'>Age (years)</label>
-            <input id = 'age' type = 'number'></input>
-            <button type = 'submit'>Add User</button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor='username'>Username</label>
+                <input id='username' type='text'></input>
+                <label htmlFor='age'>Age (years)</label>
+                <input id='age' type='number'></input>
+                <Button type='submit'>Add User</Button>
+            </form>
+        </Card>
     );
 }
 export default AddUser;
