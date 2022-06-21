2211f08ed0cac7a4b825c339da99b77d3faaaadd
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-21 10:14:01 -0500

    Form Changes

--- 01-starting-project/01-starting-project/src/components/user/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/user/AddUser.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import "./AddUser.css";
 
+const AddUser = props => {
+    return (
+
+       <form>
+        <label htmlFor="username">Username</label>
+        <input type = "text" id="username"></input >
+        <label htmlFor="age"> Age (years) </label>
+        <input type = "number" id="age"></input>
+
+        <button type = 'submit'>Add User</button>
+        </form>
+
+    );
+};
+
+export default AddUser;
