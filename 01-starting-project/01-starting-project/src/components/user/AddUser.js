4797d4c1f84b183198a6ac03273ba049cd49548b
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-21 11:28:16 -0500

    Add Age + Name Done

--- 01-starting-project/01-starting-project/src/components/user/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/user/AddUser.js
@@ -8,6 +8,17 @@ const AddUser = props => {
     const [enteredAge, setEnteredAge] = useState("");
     function addUserHandler(event) {
         event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        return;
+    }
+    if (+enteredAge < 1){
+        return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+
+
     }
     function usernameChangeHandler(event) {
         setEnteredUsername(event.target.value);
@@ -19,9 +30,9 @@ const AddUser = props => {
         <Card>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id='username' type='text' onChange = {usernameChangeHandler}></input>
+                <input id='username' type='text' value={enteredUsername} onChange = {usernameChangeHandler}></input>
                 <label htmlFor='age'>Age (years)</label>
-                <input id='age' type='number' onChange = {ageChangeHandler}></input>
+                <input id='age' type='number' value={enteredAge} onChange = {ageChangeHandler}></input>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
