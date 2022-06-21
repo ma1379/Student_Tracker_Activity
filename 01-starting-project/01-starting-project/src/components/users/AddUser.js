579c202dda96895b85e733dca6d25079a2a4fe3e
Surya Arvind <24arvins@elmbrookstudents.org>
2022-06-21 11:16:54 -0500

    Added to AddUser

--- 01-starting-project/01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/users/AddUser.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import './AddUser.css';
 import Card from '../UI/Card.js';
+import Button from '../UI/Button';
 
 
 const AddUser = props =>{
+    const{enteredUsername,setEnteredUsername}=useState("");
+    const{enteredAge,setEnteredAge}=useState("");
+
     function AddUserHandler(event){
         event.preventDefault();
 
+    }
+    function usernameChangeHandler(event) {
+        setEnteredUsername(event.target.value);
+    }
+    function ageChangeHandler(event) {
+        setEnteredAge(event.target.value);
     }
     return(
     <Card>
         <form onSubmit={AddUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username"></input>
+            <input id="username" onChange={usernameChangeHandler}></input>
             <label htmlFor="age">Age(years)</label>
-            <input id="age"></input>
-            <button type="submit">AddUser</button>
+            <input id="age" onChange={ageChangeHandler}></input>
+            <Button type="submit">AddUser</Button>
         </form>
     </Card>
     );
