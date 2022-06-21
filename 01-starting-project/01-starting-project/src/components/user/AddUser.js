bdfe7e2fe2c6977a8433a878e1da544c3e917a7e
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-21 13:58:08 -0500

    Bleh done

--- 01-starting-project/01-starting-project/src/components/user/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/user/AddUser.js
@@ -5,15 +5,25 @@ import Button from '../UI/Button.js';
 import ErrorModal from "../UI/ErrorModal.js"
 
 function AddUser(props) {
+    
     const [enteredUsername, setEnteredUsername] = useState("");
     const [enteredAge, setEnteredAge] = useState("");
+    const [error, setError] = useState();
 
     function addUserHandler(event) {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title:"Invalid Input",
+                message: "Please enter a valid name and age (non-empty values)."
+            });
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: "Invalid Age",
+                message: "Please enter valid age (age > 0)."
+            });
             return;
         }
         props.onAddUser(enteredUsername,enteredAge);
@@ -28,13 +38,15 @@ function AddUser(props) {
     function ageChangeHandler(event) {
         setEnteredAge(event.target.value);
     }
-
+    function errorHandler (){
+        setError(null);
+    }
 
 
 
     return (
         <div>
-            <ErrorModal title ="An Error Has Occured!" message = "Something went wrong!"></ErrorModal>
+            {error && <ErrorModal title ={error.title} message = {error.message} onHandleError = {errorHandler}></ErrorModal>}
 
         <Card>
             <form onSubmit={addUserHandler}>
