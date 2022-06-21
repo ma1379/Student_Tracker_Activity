b461b3cfeec4d3121ce4ada875d9a5663c772889
Surya Arvind <24arvins@elmbrookstudents.org>
2022-06-21 11:35:19 -0500

    Finished AddUser

--- 01-starting-project/01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/users/AddUser.js
@@ -10,8 +10,18 @@ const AddUser = props =>{
 
     function AddUserHandler(event){
         event.preventDefault();
+ 
+ if(enteredUsername.trim().length===0||enteredAge.trim().length===0){
+    return;
+ }
+ if(+enteredAge<1){
+    return;
+ }
+ console.log(enteredUsername,enteredAge);
+ setEnteredUsername("");
+ setEnteredAge("");
+}
 
-    }
     function usernameChangeHandler(event) {
         setEnteredUsername(event.target.value);
     }
@@ -22,9 +32,9 @@ const AddUser = props =>{
     <Card>
         <form onSubmit={AddUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" onChange={usernameChangeHandler}></input>
+            <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}></input>
             <label htmlFor="age">Age(years)</label>
-            <input id="age" onChange={ageChangeHandler}></input>
+            <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
             <Button type="submit">AddUser</Button>
         </form>
     </Card>
