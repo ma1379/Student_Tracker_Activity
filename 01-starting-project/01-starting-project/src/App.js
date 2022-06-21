61239f78c5e95d63692c8c7f4cdc78a1eb09f4be
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-21 12:21:38 -0500

    done with userList

--- 01-starting-project/01-starting-project/src/App.js
+++ 01-starting-project/01-starting-project/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUser from './components/users/AddUser.js';
+import UsersList from './components/users/UsersList.js';
 
 function App() {
   return (
     <div>
       <AddUser></AddUser>
+      <UsersList users = {[]}></UsersList>
     </div>
   );
 }
