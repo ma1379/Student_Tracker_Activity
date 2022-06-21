78605420b08533b9f6cde995ea5070074f1d0ba9
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-21 11:17:03 -0500

    changeHandlers

--- 01-starting-project/01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/01-starting-project/src/components/users/AddUser.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./AddUser.css";
 import Card from "../UI/Card.js";
 import Button from '../UI/Button.js';
 
 const AddUser = props => {
+    const[enteredUsername,setEnteredUsername] = useState("");
+    const[enteredAge,setEnteredAge] = useState("");    
     function addUserHandler(event) {
         event.preventDefault();
+    }
+    function usernameChangeHandler(event) {
+        setEnteredUsername(event.target.value);
+    }
+    function ageChangeHandler(event) {
+        setEnteredAge(event.target.value);
     }
     return (
         <Card>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id='username' type='text'></input>
+                <input id='username' type='text' onChange = {usernameChangeHandler}></input>
                 <label htmlFor='age'>Age (years)</label>
-                <input id='age' type='number'></input>
+                <input id='age' type='number' onChange = {ageChangeHandler}></input>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
